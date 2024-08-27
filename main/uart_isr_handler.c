7afbb78caeefc4ad72de16e0c72f75271ef92a88
TtijasS <tugo.strbenc@gmail.com>
2024-08-27 15:39:30 +0200

    Removed the old uart approach and replaced it with the uart_isr_handler. The uart isr handler monitors received uart data and detects patterns in the data. Upon detection of start and stop flags the message is sent to a queue, where actions can be taken based on the received message.

--- main/uart_isr_handler.c
+++ main/uart_isr_handler.c
@@ -1,9 +1,5 @@
 #include "uart_isr_handler.h"
-
 QueueHandle_t uart_event_queue_handle;
-int tmp_message_size = 0;
-uint8_t *tmp_message_buffer = NULL;
-
 QueueHandle_t queue_msg_handle;
 
 uart_config_t uart_config = {
@@ -25,20 +21,37 @@ uart_config_t uart_config = {
  * @param isr_queue_handle isr interrupt handle
  * @param isr_queue_size isr queue size (number of interrupt events to store)
  * @param intr_alloc_flags ESP_INTR_FLAG_*
+ * @return 0 OK
+ * @return -1 failed to create isre queue handle
+ * @return -2 failed to config uart params
+ * @return -3 failed to install uart driver
+ * @return -4 failed to set uart pins
  */
-void uart_init_with_isr_queue(uart_config_t *uart_config, uart_port_t port_num, int gpio_tx, int gpio_rx, int tx_buff_size, int rx_buff_size, QueueHandle_t *isr_queue_handle, int isr_queue_size, int intr_alloc_flags)
+int uart_init_with_isr_queue(uart_config_t *uart_config, uart_port_t port_num, int gpio_tx, int gpio_rx, int tx_buff_size, int rx_buff_size, QueueHandle_t *isr_queue_handle, int isr_queue_size, int intr_alloc_flags)
 {
+	const char *TAG = "UART INIT WITH Q";
+	esp_log_level_set(TAG, ESP_LOG_ERROR);
+
+	int error_code = 0; // used for debugging
 	// Create the ISR queue
 	*isr_queue_handle = xQueueCreate(isr_queue_size, sizeof(uart_event_t));
 	if (*isr_queue_handle == NULL)
 	{
-		ESP_LOGE("UART_INIT", "Failed to create ISR queue");
-		return;
+		return -1;
 	}
-
-	ESP_ERROR_CHECK(uart_param_config(port_num, uart_config));
-	ESP_ERROR_CHECK(uart_driver_install(port_num, rx_buff_size, tx_buff_size, isr_queue_size, &uart_event_queue_handle, intr_alloc_flags));
-	ESP_ERROR_CHECK(uart_set_pin(port_num, gpio_tx, gpio_rx, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
+	if ((error_code = uart_param_config(port_num, uart_config)) != 0)
+	{
+		return -2;
+	}
+	if ((error_code = uart_driver_install(port_num, rx_buff_size, tx_buff_size, isr_queue_size, isr_queue_handle, intr_alloc_flags)) != 0)
+	{
+		return -3;
+	}
+	if ((error_code = uart_set_pin(port_num, gpio_tx, gpio_rx, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE)) != 0)
+	{
+		return -4;
+	}
+	return 0;
 }
 
 /**
@@ -57,7 +70,7 @@ int uart_encapsulation_start_flag_handler(uart_port_t uart_num, int pattern_inde
 {
 	const char *TAG = "ENCAP START";
 
-	int message_size = pattern_index + ENCAPS_FLAG_SIZE;
+	int message_size = pattern_index + ENCAP_FLAG_SIZE;
 	uint8_t *tmp_buffer = (uint8_t *)malloc(message_size * sizeof(uint8_t));
 	if (tmp_buffer == NULL)
 	{
@@ -72,8 +85,9 @@ int uart_encapsulation_start_flag_handler(uart_port_t uart_num, int pattern_inde
 		return -3;
 	}
 
-	int return_value = memcmp(&tmp_buffer[pattern_index], ENCAPS_START_PAT, ENCAPS_FLAG_SIZE);
-	free(tmp_buffer); tmp_buffer = NULL;
+	int return_value = memcmp(&tmp_buffer[pattern_index], ENCAP_START_PAT, ENCAP_FLAG_SIZE);
+	free(tmp_buffer);
+	tmp_buffer = NULL;
 
 	return return_value;
 }
@@ -92,7 +106,7 @@ int uart_encapsulation_start_flag_handler(uart_port_t uart_num, int pattern_inde
  */
 int uart_encapsulation_end_flag_handler(uart_port_t uart_num, int pattern_index)
 {
-	uint8_t *tmp_buffer = (uint8_t *)malloc(ENCAPS_FLAG_SIZE*sizeof(uint8_t));
+	uint8_t *tmp_buffer = (uint8_t *)malloc(ENCAP_FLAG_SIZE * sizeof(uint8_t));
 
 	const char *TAG = "ENCAP STOP";
 
@@ -102,14 +116,15 @@ int uart_encapsulation_end_flag_handler(uart_port_t uart_num, int pattern_index)
 		return -2;
 	}
 
-	if (uart_read_bytes(uart_num, tmp_buffer, ENCAPS_FLAG_SIZE, pdMS_TO_TICKS(100)) != ENCAPS_FLAG_SIZE)
+	if (uart_read_bytes(uart_num, tmp_buffer, ENCAP_FLAG_SIZE, pdMS_TO_TICKS(100)) != ENCAP_FLAG_SIZE)
 	{
 		ESP_LOGE(TAG, "Incorrect message size");
 		return -3;
 	}
 
-	int return_value = memcmp(tmp_buffer, ENCAPS_END_PAT, ENCAPS_FLAG_SIZE);
-	free(tmp_buffer); tmp_buffer = NULL;
+	int return_value = memcmp(tmp_buffer, ENCAP_END_PAT, ENCAP_FLAG_SIZE);
+	free(tmp_buffer);
+	tmp_buffer = NULL;
 	return return_value;
 }
 
@@ -146,6 +161,37 @@ int uart_encapsulated_message_handler(uart_port_t uart_num, uint8_t *message_buf
 	return 0;
 }
 
+int message_send_to_queue(uint8_t *message, size_t message_size)
+{
+	const char *TAG = "MSG PREP SEND TO Q";
+
+	if (message == NULL)
+	{
+		ESP_LOGE(TAG, "Message null pointer passed");
+		return -1;
+	}
+
+	TaskQueueMessage_type message_to_queue = {
+		.msg_ptr = (uint8_t *)malloc(message_size * sizeof(uint8_t)),
+		.msg_size = message_size};
+
+	if (message_to_queue.msg_ptr == NULL)
+	{
+		ESP_LOGE(TAG, "Failed to malloc queued message buffer");
+		return -2;
+	}
+	// Copy message to the new address
+	memcpy(message_to_queue.msg_ptr, message, message_size * sizeof(uint8_t));
+
+	// Send the message to queue
+	if (xQueueSend(queue_msg_handle, &message_to_queue, portMAX_DELAY) != pdTRUE)
+	{
+		free(message_to_queue.msg_ptr);
+		return -3;
+	}
+	return 0;
+}
+
 /**
  * @brief Handles the state of the received message and manages the UART communication flow
  *
@@ -164,20 +210,28 @@ int uart_encapsulated_message_handler(uart_port_t uart_num, uint8_t *message_buf
  * @return -2 message size < 1
  * @return -3 failed to malloc message buffer
  * @return -4 failed to to read the message from RX buffer
- * @return -5 bad stop flag
- * @return -6 failed malloc message buffer for queue message
- * @return -5 failed to send the message copy to queue
+ * @return -5 failed malloc message buffer for queue message
+ * @return -6 failed to send the message copy to queue
+ * @return -7 bad stop flag
  */
 int uart_encapsulation_handler(uart_port_t uart_num, int *encap_state, int *pattern_index)
 {
+	enum encap_states
+	{
+		START_FLAG_CONFIRMATION,
+		END_FLAG_CONFIRMATION,
+	};
 	const char *TAG = "ENCAP HANDLER";
 	uint8_t *tmp_message_buf = NULL;
-	int tmp_message_size = 0;
-	int error_code = 0; // To store the error code and then return it
+	int tmp_message_size = 0; // To store corrected message size
+	int error_code = 0;		  // To store the error code and then return it
 
-	// Parse START FLAG
-	if (*encap_state == 0)
+	switch (*encap_state)
 	{
+	case START_FLAG_CONFIRMATION:
+		/**
+		 * @brief Read and check the START FLAG
+		 */
 		if (uart_encapsulation_start_flag_handler(UART_NUM, *pattern_index) == 0)
 		{
 			(*encap_state)++;
@@ -187,74 +241,63 @@ int uart_encapsulation_handler(uart_port_t uart_num, int *encap_state, int *patt
 		{
 			ESP_LOGE(TAG, "Bad start flag");
 			error_code = -1; // Error code for bad start flag
-			goto cleanup;
+			goto memcleanup;
 		}
-	}
-	else if (*encap_state == 1)
-	{
+		break;
+
+	case END_FLAG_CONFIRMATION:
 		tmp_message_size = *pattern_index - 1;
 
 		if (tmp_message_size < 1)
 		{
-			ESP_LOGE(TAG, "Message size < 1");
+			ESP_LOGE(TAG, "Message size %d is less than 1", tmp_message_size);
 			error_code = -2; // Error code for invalid message size
-			goto cleanup;
+			goto memcleanup;
 		}
 
 		tmp_message_buf = (uint8_t *)malloc(tmp_message_size * sizeof(uint8_t));
 		if (tmp_message_buf == NULL)
 		{
-			ESP_LOGE(TAG, "Failed to malloc tmp data buffer");
+			ESP_LOGE(TAG, "Failed to malloc tmp message buffer");
 			error_code = -3; // Error code for memory allocation failure
-			goto cleanup;
+			goto memcleanup;
 		}
 
+		/**
+		 * @brief Read the MESSAGE bytes
+		 */
 		if (uart_encapsulated_message_handler(UART_NUM, tmp_message_buf, tmp_message_size) == 0)
 		{
-			(*encap_state)++;
 			*pattern_index -= tmp_message_size;
 		}
 		else
 		{
 			ESP_LOGE(TAG, "Failed to receive message");
 			error_code = -4; // Error code for failed message receipt
-			goto cleanup;
+			goto memcleanup;
 		}
-	}
 
-	if ((*encap_state == 2) && (uart_encapsulation_end_flag_handler(UART_NUM, *pattern_index) == 0))
-	{
-		TaskQueueMessage_type message_to_queue = {
-			.msg_ptr = (uint8_t *)malloc(tmp_message_size * sizeof(uint8_t)),
-			.msg_size = tmp_message_size};
-
-		if (message_to_queue.msg_ptr == NULL)
+		/**
+		 * @brief Read and check STOP FLAG
+		 */
+		if (uart_encapsulation_end_flag_handler(UART_NUM, *pattern_index) == 0)
 		{
-			error_code = -6; // Error code for queue message memory allocation failure
-			goto cleanup;
+			if (message_send_to_queue(tmp_message_buf, tmp_message_size) != 0)
+			{
+				ESP_LOGE(TAG, "Failed to send message to queue");
+			}
 		}
-		memcpy(message_to_queue.msg_ptr, tmp_message_buf, tmp_message_size * sizeof(uint8_t));
-
-		if (xQueueSend(queue_msg_handle, &message_to_queue, portMAX_DELAY) != pdTRUE)
+		else
 		{
-			free(message_to_queue.msg_ptr);
-			error_code = -7; // Error code for queue send failure
-			goto cleanup;
+			ESP_LOGE(TAG, "Bad stop flag");
+			error_code = -7; // Error code for failed stop flag
 		}
-		free(tmp_message_buf); // Free only if successful
-		tmp_message_buf = NULL;
-		*encap_state = 0; // Move state forward only on success
-		uart_pattern_queue_reset(UART_NUM, UART_PAT_QUEUE_SIZE);
-		return 0; // Successful execution
-	}
-	else
-	{
-		ESP_LOGE(TAG, "Failed stop flag");
-		error_code = -5; // Error code for failed stop flag
-		goto cleanup;
-	}
+		break;
 
-cleanup:
+	default:
+		break;
+	}
+memcleanup:
 	if (tmp_message_buf != NULL)
 	{
 		free(tmp_message_buf);
@@ -277,9 +320,6 @@ void task_uart_isr_monitoring(void *params)
 		// Receive the entire uart_event_t structure from the queue
 		if (xQueueReceive(uart_event_queue_handle, (void *)&uart_event, portMAX_DELAY) == pdTRUE)
 		{
-			// Clear the buffer
-			ESP_LOGI(TAG, "[EVENT]: %d, [SIZE]: %d", uart_event.type, uart_event.size);
-
 			switch (uart_event.type)
 			{
 			/**
@@ -287,37 +327,22 @@ void task_uart_isr_monitoring(void *params)
 			 *
 			 * Multiple identical characters of specific type detected in a row.
 			 * Number of chars is defined with macro UART_PATTERN_SIZE
-			 *
 			 */
 			case UART_PATTERN_DET:
-			{
-				const char *descriptor = "[UART_PAT]: "; /*!<  message description*/
-
 				int pattern_index = uart_pattern_pop_pos(UART_NUM);
-				ESP_LOGI(descriptor, "Pat index: %d", pattern_index);
 				if (pattern_index == -1)
 				{
 					ESP_LOGE(TAG, "Pattern index -1");
 					break;
 				}
-
-				error_flag = uart_encapsulation_handler(UART_NUM, &encapsulation_counter, &pattern_index);
-
-				if (error_flag == 0)
+				if ((error_flag = uart_encapsulation_handler(UART_NUM, &encapsulation_counter, &pattern_index)) < 0)
 				{
-					ESP_LOGI(TAG, "Successfull message: %d", error_flag);
-				}
-				else
-				{
-					ESP_LOGE(TAG, "Error flag: %d", error_flag);
+					ESP_LOGE(TAG, "Uart encapsulation handler error %d", error_flag);
 				}
 				break;
-			}
+
 			default:
-			{
-				ESP_LOGI(TAG, "Default switch state");
 				break;
-			}
 			}
 		}
 	}
@@ -325,10 +350,12 @@ void task_uart_isr_monitoring(void *params)
 	vTaskDelete(NULL);
 }
 
-void task_receive_message(void *params)
+void task_queue_msg_handler(void *params)
 {
-	const char *TAG = "RECEIVE MSG";
+	const char *TAG = "TSK QUEUE MSG HANDL";
 	TaskQueueMessage_type enqueued_message;
+
+	const char *SAMPLING_STARTED = "Sampling started";
 
 	while (1)
 	{
@@ -336,10 +363,12 @@ void task_receive_message(void *params)
 		{
 			if (enqueued_message.msg_ptr != NULL)
 			{
-
-				ESP_LOGI(TAG, "Sending message");
-				// Write the message
-				if(uart_write_bytes(UART_NUM, enqueued_message.msg_ptr, enqueued_message.msg_size) == -1)
+				if (memcmp(enqueued_message.msg_ptr, "START", strlen("START")))
+				{
+					uart_write_bytes(UART_NUM, SAMPLING_STARTED, strlen(SAMPLING_STARTED));
+					// xTaskNotifyGive(notif_data_sampling)
+				}
+				else if (uart_write_bytes(UART_NUM, enqueued_message.msg_ptr, enqueued_message.msg_size) == -1)
 				{
 					ESP_LOGE(TAG, "Failed to write bytes");
 				}
